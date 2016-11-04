2583ff8ae1a536b7210b7a13c7bb10c1dc25f3df
oplawsze <oskar.plawszewski@nokia.com>
2016-11-04 21:59:04 +0100

    cleaning

--- luizalos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LuizalosConfig(AppConfig):
-    name = 'luizalos'
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'blog',
     'galery',
     'events',
-    'luizalos',
 ]
 
 MIDDLEWARE_CLASSES = [
