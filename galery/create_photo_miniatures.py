a2f10e6b1b7c6c6d62091aadae0a4a586eff2791
oplawsze <oskar.plawszewski@nokia.com>
2016-12-06 22:20:40 +0100

    fill db WIP

--- galery/create_photo_miniatures.py
+++ galery/create_photo_miniatures.py
@@ -24,8 +24,9 @@ def fill_db():
     Returns:
 
     """
+    Photo_miniture.objects.all().delete()
     for photo in Photo.objects.all():
-        save_path = r'C:\Users\oplawsze\media_files'
+        save_path = MEDIA_FILES_PATH
         img = Image.open(photo.image)
         img.thumbnail(SIZE, Image.ANTIALIAS)
         name_of_file = photo.title + 'mini.jpg'
