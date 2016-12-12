10363fc5f7aab1dbd38004351959721e141145c1
oplawsze <oskar.plawszewski@nokia.com>
2016-12-12 22:36:24 +0100

    working with signals

--- blog/signals.py
+++ blog/signals.py
@@ -1,9 +1,9 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from galery.create_photo_miniatures import fill_db
+from galery.fill_db import fill_db
 from galery.models import Photo
 
 
 @receiver(post_save, sender=Photo)
-def handle_pronto_for_user_statistics(instance, **kwargs):
+def handle_pronto_for_user_statistics():
     fill_db()
